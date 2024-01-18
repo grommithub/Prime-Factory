c9d819627201e8ed3597483f4b9e0276ceb1caaf
Dan Burt <gromitzy@gmail.com>
2024-01-18 14:19:32 +0100

    Fast!

--- PrimeFactory/PrimeFactory.cpp
+++ PrimeFactory/PrimeFactory.cpp
@@ -6,28 +6,46 @@
 #include <algorithm>
 #include <chrono>
 
+#include <thread>
+#include <mutex>
+
+
+
+
+inline bool no_common_factors(int num, std::vector<int>& primes)
+{
+	return std::none_of(primes.begin(), std::find_if(primes.begin(), primes.end(), [num](auto a) { return a * a > num; }), [num](unsigned int a) { return num % a == 0; });
+}
+
 int main()
 {
+	auto start_time = std::chrono::high_resolution_clock::now();
 
-	std::vector<unsigned int> primes;
+	std::vector<std::thread> threads;
 
-	auto biggest_possible_prime_it = primes.begin();
+	std::vector<int> primes;
+	primes.push_back(2);
+	primes.push_back(3);
 
-	for (unsigned int i = 2; i < 1'000'000'000; i++)
+	for (int i = 6; i < 1'000'000'000; i += 6)
 	{
-		biggest_possible_prime_it = std::find_if(biggest_possible_prime_it, primes.end(), [i](auto a) { return a > sqrt(i); });
-		if (std::none_of(primes.begin(), biggest_possible_prime_it, [i](unsigned int a) { return i % a == 0; }))
+		for (int j = -1; j <= 1; j += 2)
 		{
-			primes.push_back(i);
-			if (primes.size() % 100 == 0)
+			if(no_common_factors(i + j, primes))
 			{
-				std::cout << primes.size() << '\n';
+				primes.push_back(i + j);
+				if (primes.size() % 1'000'000 == 0)
+				{
+					std::cout << primes.size() << " Biggest prime so far: " << primes.back() << '\n';
+				}
 			}
 		}
 	}
 
-	int* a = (int*)malloc(sizeof(int));
+	auto end_time = std::chrono::high_resolution_clock::now();
 
+	std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+	std::cout << "Exection time : " << duration.count() << " seconds\n";
 
 	std::cout << "Done! " << primes.size() << '\n';
 
