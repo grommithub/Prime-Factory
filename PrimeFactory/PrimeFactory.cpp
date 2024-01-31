ccffc1837cac610df4c4aa78dfc5d55876709e24
Dan <44238640+grommithub@users.noreply.github.com>
2024-01-31 17:38:36 +0100

    Parallell printing!

--- PrimeFactory/PrimeFactory.cpp
+++ PrimeFactory/PrimeFactory.cpp
@@ -8,8 +8,10 @@
 
 #include <thread>
 #include <mutex>
+#include "PrimeFactory.h"
+#include <execution>
 
-
+#include <functional>
 
 
 inline bool no_common_factors(int num, std::vector<int>& primes)
@@ -17,44 +19,107 @@ inline bool no_common_factors(int num, std::vector<int>& primes)
 	return std::none_of(primes.begin(), std::find_if(primes.begin(), primes.end(), [num](auto a) { return a * a > num; }), [num](unsigned int a) { return num % a == 0; });
 }
 
-int main()
+
+std::chrono::duration<double> TimeFunction(std::function<void(void)> func)
 {
 	auto start_time = std::chrono::high_resolution_clock::now();
+	func();
+	auto end_time = std::chrono::high_resolution_clock::now();
+	return std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+}
 
-	std::vector<std::thread> threads;
+
+int main()
+{
 
 	std::vector<int> primes;
 	primes.push_back(2);
 	primes.push_back(3);
 
-	for (int i = 6; i < 1'000'000'000; i += 6)
+	primes.reserve(100'000);
+
+	auto start_time = std::chrono::high_resolution_clock::now();
+
+	for (int i = 6; i < 1'000'000; i += 6)
 	{
 		for (int j = -1; j <= 1; j += 2)
 		{
-			if(no_common_factors(i + j, primes))
+			if (no_common_factors(i + j, primes))
 			{
 				primes.push_back(i + j);
-				if (primes.size() % 1'000'000 == 0)
-				{
-					std::cout << primes.size() << " Biggest prime so far: " << primes.back() << '\n';
-				}
+				//if (primes.size() % 1'000'000 == 0)
+				//{
+				//	std::cout << primes.size() << " Biggest prime so far: " << primes.back() << '\n';
+				//}
 			}
 		}
 	}
-
 	auto end_time = std::chrono::high_resolution_clock::now();
 
-	std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
-	std::cout << "Exection time : " << duration.count() << " seconds\n";
+	std::cout << "Par time : " << TimeFunction([primes]() { std::for_each(std::execution::par, primes.begin(), primes.end(), [](const auto p) { std::cout << std::to_string(p) + '\n'; }); });
 
-	std::cout << "Done! " << primes.size() << '\n';
 
-#if 0
-	for (auto prime : primes)
-	{
-		std::cout << prime << '\n';
-	}
-#endif // 0
+
+
+	//std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+
+	//std::cout << "Done! " << primes.size() << '\n';
+	//std::cout << "Exection time : " << duration.count() << " seconds\n";
+
+
+
+
+	//std::vector<std::thread> threads;
+	//auto thread_count = std::thread::hardware_concurrency();
+
+	//std::vector<int> primesB;
+	//primes.push_back(2);
+	//primes.push_back(3);
+
+
+	//int current = 3; int high = 9;
+	//while (current < 1'000'000)
+	//{
+	//	int chunkSize = (high - current) / thread_count;
+	//	
+	//	for (int i = 0; i < thread_count; i++)
+	//	{
+	//		auto start = current + 
+	//	}
+	//	
+	//	threads.emplace_back()
+
+
+	//}
+
+
+	//for (auto& thread : threads)
+	//{
+	//	thread.join();
+	//}
+
+
+
 
 }
+
+
+
+//void add_primes_in_range(std::vector<int>& primes, int start, int end)
+//{
+//	for (int i = start; i < end; i += 6)
+//	{
+//		for (int j = -1; j <= 1; j += 2)
+//		{
+//			if (no_common_factors(i + j, primes))
+//			{
+//				primes.push_back(i + j);
+//				if (primes.size() % 1'000'000 == 0)
+//				{
+//					std::cout << primes.size() << " Biggest prime so far: " << primes.back() << '\n';
+//				}
+//			}
+//		}
+//	}
+//}
 
---
+++ PrimeFactory/PrimeFactory.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void add_primes_in_range(std::vector<int>& primes);
